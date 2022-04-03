39d31d58a9bd7184e30b047d50278e4e7f12bb28
bknownothing <102900542+bknownothing@users.noreply.github.com>
2022-04-03 17:36:29 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -465,11 +465,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '一般路过荠菜叶？';
+        if (cps <= 8) return '合格荠菜叶';
+        if (cps <= 10)  return '漂亮的荠菜叶';
+        if (cps <= 15) return '您就是电脑少女？';
+        return '极彩夜单推';
     }
 
     function toStr(obj) {
